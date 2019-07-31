974c517276a05f0844b28753949fa02496726cbe
Anthony DiRusso <ossurid@gmail.com>
2019-07-31 16:20:42 -0400

    adds express validator check for log in process

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-//@route      GET api/Posts
+//@route      POST api/Posts
 //@desc       test route
 //@access     Public
-router.get('/', (req, res) => res.send('Posts route'));
+router.post('/', (req, res) => res.send('Posts route'));
 
 module.exports = router;
